18228833eb0a6a1f4ea939888587958ffcbcdf87
Sourabh Dey <146729006+CodeRreaper69@users.noreply.github.com>
2024-04-06 12:18:28 +0530

    Update run_sudo.sh

--- run_sudo.sh
+++ run_sudo.sh
@@ -45,13 +45,15 @@ sudo apt install python3
 sudo apt-get install python3-pip
 pip install setuptools
 sudo python3 setup.py install
+sleep 1
+cd 
+sudo rm -rf karma
 
 # Prompt the user to run the karma9 command
 echo -e "\033[1;32mInstallation completed successfully.\033[0m"
 echo -e "\033[1;32mYou can now run the 'karma9' command to start the application.\033[0m"
-sleep 0.7
-cd 
-sudo rm -rf karma
+sleep 2
+clear
 
 # Clean up (optional)
 # rm -rf karma9  # Uncomment this line to remove the extracted folder after installation
